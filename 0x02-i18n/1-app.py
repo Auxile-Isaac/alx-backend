a1b96946230ede07bf33d22b9f4c81b409de9893
Auxile_Isaac <auxile.isaac@gmail.com>
2023-02-28 23:42:05 +0200

    correction

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
-""" Basic Flask app, Basic Babel setup """
-from flask import Flask, render_template
+"""A Basic Flask app.
+"""
 from flask_babel import Babel
+from flask import Flask, render_template
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
-babel = Babel(app)
-""" instantiate the Babel object """
-
-
-class Config(object):
-    """ config class """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
 app.config.from_object(Config)
-""" Use that class as config for Flask app """
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
 @app.route('/')
-def root():
-    """ basic Flask app """
-    return render_template("1-index.html")
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('1-index.html')
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
